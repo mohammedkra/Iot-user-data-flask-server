e3300d828703be9eabec9d05533b76ce77e698df
Mohammed Mohammad <mohammem@axis.com>
2021-12-08 15:25:56 +0100

    ongoing:packaging the application

--- HomeSensorProject/exportKeys.py
+++ HomeSensorProject/exportKeys.py
@@ -2,7 +2,7 @@ from rsa.key import PrivateKey
 
 #THIS METHOD EXPORTS PRIVATE KEYS FROM THE STORAGE
 def readPrivKey():
-    f = open("sessions_priv_key", "rb")
+    f = open("session_priv_key", "rb")
     key = f.read()
     cipher = PrivateKey.load_pkcs1(key)
     f.close()
